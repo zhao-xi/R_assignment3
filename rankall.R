65b197a28ad44c2e6ef11180644dc89975c71ef3
zhao-xi <zhaoxi0418@outlook.com>
2018-07-12 22:30:02 +0900

    finally rakall seems work fine

---
+++ rankall.R
@@ -0,0 +1,49 @@
+rankall <- function(outcome, num = "best"){
+        ## The data frame that is going to be returned
+        result <- data.frame(matrix(nrow = 0, ncol = 2))
+        colnames(result) <- c("hospital","state")
+        
+        ## Read outcome data
+        outcome_data <- read.csv("outcome-of-care-measures.csv",
+                                 colClasses = "character")
+        
+        ## Check that outcome is valid
+        outcome_vect <- c("heart attack", "heart failure", "pneumonia")
+        state_vect <- as.character(unique(outcome_data[["State"]]))
+        if(!(outcome %in% outcome_vect)){
+                message("Error in best("+state+", "+outcome+") : "
+                        +"invalid outcome")
+                stop()
+        }
+        
+        ## Return the data frame with the hospital names and the (abbreviated)
+        ## state name
+        if(outcome=="heart attack"){
+                outcome <- 
+                        "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+        }
+        if(outcome=="heart failure"){
+                outcome <-
+                        "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+        }
+        if(outcome=="pneumonia"){
+                outcome <-
+                        "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        }
+        
+        data_list <- split(outcome_data, outcome_data$State)
+        data_list <- lapply(data_list,function(y) y<-y[order(y[[outcome]]),])
+     
+        for(x in state_vect){
+                if(num == "best") num <- 1
+                if(num == "worst") num <- nrow(data_list[[x]])
+                
+                hospital <- as.character(data_list[[x]]$Hospital.Name[num])
+                state <- as.character(data_list[[x]]$State[1])
+                new_row <- data.frame(hospital, state)
+                colnames(new_row) <- c("hospital","state")
+                result <- rbind(result, new_row)
+        }
+        result <- result[order(as.character(result$state)),]
+        result
+}
